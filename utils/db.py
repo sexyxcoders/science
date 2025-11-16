639be6ec0c37fdd1d0f791a2d5914f854838eaa3
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-11-16 19:43:14 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,17 @@
+from pymongo import MongoClient
+from config import MONGO_URI
 
+# ------------------------
+# MongoDB Client
+# ------------------------
+client = MongoClient(MONGO_URI)
+db = client["science_quiz_bot"]
+
+# ------------------------
+# Collections
+# ------------------------
+groups_col = db["groups"]         # Stores group info: running quiz, timer
+users_col = db["users"]           # Stores user points, coins, etc.
+sessions_col = db["sessions"]     # Stores current quiz sessions per group
+questions_col = db["questions"]   # Stores all quiz questions
+admins_col = db["admins"]         # Stores bot admins
