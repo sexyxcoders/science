6d7516e9daebb361457e0d1d5f8926f670a1b709
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-11-16 21:08:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import os
 # ------------------------
 # Pyrogram Bot Configuration
 # ------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")          # Bot token from BotFather
-API_ID = int(os.environ.get("API_ID"))           # Your Telegram API ID
-API_HASH = os.environ.get("API_HASH")           # Your Telegram API HASH
-OWNER_ID = int(os.environ.get("OWNER_ID"))       # Telegram user ID of the bot owner
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7901218073:AAEXbFvRpm6c2IHImk9n8FjJm1XhNJ1TR2U")          # Bot token from BotFather
+API_ID = int(os.environ.get("API_ID", "22657083"))           # Your Telegram API ID
+API_HASH = os.environ.get("API_HASH", "d6186691704bd901bdab275ceaab88f3")           # Your Telegram API HASH
+OWNER_ID = int(os.environ.get("OWNER_ID", "8449801101"))       # Telegram user ID of the bot owner
 
 # ------------------------
 # MongoDB Configuration
 # ------------------------
-MONGO_URI = os.environ.get("MONGO_URI")         # MongoDB connection URI
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://pikachuxivan_db_user:pikachuxivan@cluster0.9c3hko7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")         # MongoDB connection URI
 
 # ------------------------
 # Question Channel (Optional)
 # ------------------------
 # All questions can be synced to this channel for backup or public reference
-QUESTION_CHANNEL = os.environ.get("QUESTION_CHANNEL")  # e.g., "@my_question_channel"
+QUESTION_CHANNEL = os.environ.get("QUESTION_CHANNEL", "@Sciencedatabased")  # e.g., "@my_question_channel"
